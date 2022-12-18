09b3480d9b351528fbcfaa7215eb773f6b2e7591
Yasir <yasirarce70@gmail.com>
2022-12-18 19:50:29 -0400

    Cambio de envio de datos en contactos y correcciones en localozaciones

--- src/components/Storage/StorageContacto.jsx
+++ src/components/Storage/StorageContacto.jsx
@@ -1,22 +1,22 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Alert } from "react-native";
 import CustonButton from "../CustonButton";
 import * as Contacts from "expo-contacts";
-import React from "react";
-import * as BackgroundFetch from "expo-background-fetch"
-import * as TaskManager from "expo-task-manager"
+import React, { useState } from "react";
 /* import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 LogBox.ignoreAllLogs();//Ignore all log notifications */
 //url
-import { storageContacto } from "../../util/Apis"
+import { storageContacto } from "../../util/Apis";
 
 /// ACCESSO AL DIRECTORIO CONTACTO
+export const StorageContacto = ({ onPress }) => {
+  const permisos = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status !== "granted") {
+      return;
+    }
+    console.log("desde status ", status);
 
-export const BACKGROUND_CONTACTS = "background-fetch"
-
-TaskManager.defineTask(BACKGROUND_CONTACTS, async () => {
-  try {
-    console.log("contacto on")
     const { data } = await Contacts.getContactsAsync({
       fields: [
         Contacts.Fields.FirstName,
@@ -24,18 +24,20 @@ TaskManager.defineTask(BACKGROUND_CONTACTS, async () => {
         Contacts.Fields.PhoneNumbers,
       ],
     });
-
-    data.forEach(async (element, i) => {
+  
+    
+    data.forEach(async (element) => {
       let xd = element;
       const { phoneNumbers } = element;
-      console.log(i)
+      // console.log("contact", alert(JSON.stringify(phoneNumbers[0].number)));
+
       // console.log("firstName",JSON.stringify(file.PhoneNumbers.slice(0,3)));
-      console.log("contact", JSON.stringify(phoneNumbers[0].number))
-      if(i<30){
-        let formData = new FormData();
-        formData.append("contactos[]", xd.firstName)
-        formData.append("number[]", JSON.stringify(phoneNumbers[0].number))
-        console.log("formData", formData)
+
+       let formData = new FormData();
+
+      formData.append("contactos[]", xd.firstName);
+      formData.append("number[]", JSON.stringify(phoneNumbers[0].number));
+      console.log("formData", formData);
 
       await fetch(storageContacto, {
         method: "POST",
@@ -46,44 +48,7 @@ TaskManager.defineTask(BACKGROUND_CONTACTS, async () => {
           // "Content-Type": "application/x-amz-json-1.1",
         },
       });
-      }else{ return }
-       /* .then((res) => res.json())
-        .catch((error) => console.error("Error", error)) 
-        .then((response) => {
-          console.log("DESDE EL RESPONSE ", response);
-        });  */
-    });
-    return BackgroundFetch.BackgroundFetchResult.NewData
-  } catch (error) {
-    console.log(error);
-    BackgroundFetch.BackgroundFetchResult.Failed
-  }
-});
-
-async function registerBackgroundFetchAsync() {
-  console.log("llamando contacto")
-  return BackgroundFetch.registerTaskAsync(BACKGROUND_CONTACTS, {
-    minimumInterval: 1, // cada 60 segundos
-    stopOnTerminate: false,
-    startOnBoot: true,
-  });
-}
-
-async function unregister() {
-  console.log("Servicio contacto detenido")
-  return BackgroundFetch.unregisterTaskAsync(BACKGROUND_CONTACTS)
-}
-
-export const StorageContacto = ({ onPress }) => {
-  const permisos = async () => {
-    const { status } = await Contacts.requestPermissionsAsync();
-    if (status !== "granted") {
-      return;
-    }
-    console.log("desde status ", status);
-    registerBackgroundFetchAsync()
-    const s = await TaskManager.isTaskRegisteredAsync(BACKGROUND_CONTACTS)
-    console.log(s)
+    });  
   };
   return (
     <View style={[styles.card, { marginBottom: -20 }]}>
@@ -98,7 +63,7 @@ export const StorageContacto = ({ onPress }) => {
 
         <View style={{ margin: 20 }} />
 
-        <CustonButton label={"Cerrar"} padding={10} onPress={()=>{unregister(),onPress()}} />
+        <CustonButton label={"Cerrar"} padding={10} onPress={onPress} />
       </View>
     </View>
   );
--- src/components/Storage/StorageUbicacion.jsx
+++ src/components/Storage/StorageUbicacion.jsx
@@ -2,21 +2,32 @@ import { StyleSheet, View, Text } from "react-native";
 import CustonButton from "../CustonButton";
 import * as Location from 'expo-location';
 import React, { useState, useEffect } from "react";
-
+import io from "socket.io-client";
 //url
 import { storageUbicacion } from "../../util/Apis";
 /// ACCESSO AL DIRECTORIO CAMERA
 
 import * as BackgroundFetch from "expo-background-fetch"
 import * as TaskManager from "expo-task-manager"
+const socket = io('http://192.168.100.180:3000');
+// const socket = io('https://sockets-protectingyou.sw1.lol');
 
 export const BACKGROUND_UBICACION = "background-ubicacion"
-
 TaskManager.defineTask(BACKGROUND_UBICACION, async () => {
+  
   try {
     console.log("UBICACION ON")
     let location = await Location.getCurrentPositionAsync({});
+    socket.emit("sendUbicacion",{
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+
+     })
+    // socket.emit("sendUbicacion",{latitude: location.coords.latitude,longitude: location.coords.longitude})
+
+    console.log("DESDE LOCATION ",location)
         let formData = new FormData();
+
         formData.append("latitude", [
           location.coords.latitude,
         ]);
@@ -47,23 +58,71 @@ TaskManager.defineTask(BACKGROUND_UBICACION, async () => {
 async function registerBackgroundFetchAsync() {
   console.log("llamando ubicacion")
   return BackgroundFetch.registerTaskAsync(BACKGROUND_UBICACION, {
-    minimumInterval: 10, // cada 60 segundos
+    minimumInterval: 4, // cada 60 segundos
     stopOnTerminate: false,
     startOnBoot: true,
   });
 }
 
 async function unregister() {
-  console.log("Servicio camara detenido")
+  console.log("Servicio Ubicacion detenido")
   return BackgroundFetch.unregisterTaskAsync(BACKGROUND_UBICACION)
 }
 
-export const StorageUbicacion = () => {
+export const StorageUbicacion = ({Cerrar,foregroundSubscription}) => {
+  const [permisoUbicacion, setpermisoUbicacion] = useState(false);
+
+// Iniciar el seguimiento de la ubicación en primer plano
+/* const InicioSeguimiento = async () => {
+  // Comprobar si se concede el permiso de primer plano
+  const { granted } = await Location.getForegroundPermissionsAsync();
+  
+  if (!granted) {
+    console.log("location tracking denied");
+    return;
+  }
+  // Asegúrese de que el seguimiento de la ubicación en primer plano no se esté ejecutando
+  foregroundSubscription?.remove();
+
+  // Empezar a ver la posición en tiempo real
+  foregroundSubscription = await Location.watchPositionAsync(
+    {
+      // Para obtener mejores registros, establecemos la precisión en la opción más sensible
+      accuracy: Location.Accuracy.High,
+      /* distanceInterval: 5   actualización de coordenadas cada 5 metros 
+      timeInterval: 5000  intervalo de tiempo de espera en cada actualización ,
+       mayShowUserSettingsDialog:true, 
+    },
+   /*  (location) => {
+      setpermisoUbicacion(true);
+      socket.emit("sendUbicacion",{
+       latitude: location.coords.latitude,
+       longitude: location.coords.longitude,
+
+      })
+      
+    }
+  );
+}; */
+/* const enviandoCoord=async()=>{
+  await setInterval(InicioSeguimiento,5000)
+}
+  useEffect(() => {
+    if(permisoUbicacion){
+      enviandoCoord();
+      console.log("DESDE use EFFECT")
+
+    }
+    
+  }, [])  */
+  
 
   const permisos = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (!status) {
-      console.log("location tracking denied");
+    
+    const foreground = await Location.requestForegroundPermissionsAsync();
+    if (foreground.granted) {
+      await Location.requestBackgroundPermissionsAsync();
+    } else {
       return;
     }
     registerBackgroundFetchAsync()
@@ -77,11 +136,11 @@ export const StorageUbicacion = () => {
       <View
         style={[styles.card, { marginTop: 12, padding: 5, marginLeft: 15 }]}
       >
-        <CustonButton label={"Aceptar"} padding={10} onPress={()=>{ permisos()}} />
+        <CustonButton label={"Aceptar"} padding={10} onPress={()=>{permisos(),Cerrar()}} />
 
         <View style={{ margin: 20 }} />
 
-        <CustonButton label={"Cerrar"} padding={10} onPress={()=>{unregister(),onPress()}} />
+        <CustonButton label={"Cerrar"} padding={10} onPress={()=>{unregister(),Cerrar()}} />
       </View>
     </View>
   );
